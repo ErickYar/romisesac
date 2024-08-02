207637964bccf0eb774f09c502c7bf384b030cfe
ErickYar <erickyb1985@gmail.com>
2024-08-02 01:46:30 -0500

    todo esta bien

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const menuToggle = document.querySelector(".menu-toggle");
 const navigation = document.querySelector(".navigation");
 menuToggle.onclick = function () {
@@ -18,25 +18,5 @@ list.forEach(
    function (item) {
       item.addEventListener('click', activarLink)
    }
-=======
-const menuToggle = document.querySelector(".menu-toggle");
-const navigation = document.querySelector(".navigation");
-menuToggle.onclick = function () {
-   navigation.classList.toggle('open')
-}
-const list = document.querySelectorAll('.list');
-function activarLink() {
-   list.forEach((item) => {
-      item.classList.remove("active");
-      this.classList.add("active")
-   })
-}
-// Esta es la forma abreviada de la función 
-// list.forEach((item) => item.addEventListener('click', activarLink));
 
-list.forEach(
-   function (item) {
-      item.addEventListener('click', activarLink)
-   }
->>>>>>> 4330375425947bc222c0d02c2157b5ce9ebe09d1
 )
